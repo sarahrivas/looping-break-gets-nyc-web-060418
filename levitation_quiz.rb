c71394a441197cda9b73e73638138f4c98cc2688
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-26 01:07:06 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,7 +3,7 @@ def levitation_quiz
 	loop do 
 	  puts "What is the spell that enacts levitation?"
 	  answer = gets.chomp 
-	  break if == "Windgardium Leviosa"
+	  breakif "Windgardium Leviosa"
 	end
 	"You passed the quiz!"
 end
